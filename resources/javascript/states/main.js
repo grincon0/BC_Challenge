0c73bb42be8f55082f8c7519b807dd95cfa36ba4
Wake <georgerincon2@gmail.com>
2018-11-05 03:31:09 -0500

    tested sprite animations and game physics

--- resources/javascript/states/main.js
+++ resources/javascript/states/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(600,400, Phaser.AUTO);
+var game = new Phaser.Game(1000,800, Phaser.AUTO);
 
 game.state.add('state1', demo.state1);
 game.state.add('state2', demo.state2);
--- resources/javascript/states/state1.js
+++ resources/javascript/states/state1.js
@@ -3,10 +3,26 @@ var demo = window.demo || (window.demo = {});
 demo.state1 = function (){};
 
 demo.state1.prototype = {
-    preload: function() {},
+    preload: function() {
+        game.load.spritesheet('test', 'resources/art/spritesheets/test_idle.png',123,204,8);
+        
+    },
     create: function () {
-        game.stage.backgroundColor = '#4286f4';
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.arcade.gravity.y = 100;
+        let player = game.add.sprite(492,408, 'test');
+        game.physics.enable( [ player ], Phaser.Physics.ARCADE);
+        player.anchor.setTo(0.5,0.5);
+        player.body.collideWorldBounds = true;
+        
+        player.body.gravity.y = 200;
+        let idle = player.animations.add('idle');
+
+        player.animations.play('idle',13 ,true);
+        console.log('animation is working');
         console.log('state1');
     },
-    update : function (){}
+    update : function (){
+
+    }
 };
